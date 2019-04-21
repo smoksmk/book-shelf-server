70883d3714aaf9bbc249903ace7785c7bf5868f7
Игорь Чепко <smoksmk@gmail.com>
2019-04-21 17:58:23 +0300

    add environment config

--- app/config.py
+++ app/config.py
@@ -1,9 +1,14 @@
-APP_NAME = 'book-shelf'
+from app.utils import get_config
 
-PG_HOST = 'localhost'
-PG_USERNAME = 'book_shelf'
-PG_PASSWORD = 'book_shelf'
-PG_SCHEMA = PG_USERNAME
-PG_PORT = 5432
+env = get_config()
+
+print(env['PG_HOST'])
+APP_NAME = env['APP_NAME']
+
+PG_HOST = env['PG_HOST']
+PG_USERNAME = env['PG_USERNAME']
+PG_PASSWORD = env['PG_PASSWORD']
+PG_SCHEMA = env['PG_SCHEMA']
+PG_PORT = env['PG_PORT']
 
 URL = f'postgresql://{PG_USERNAME}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_SCHEMA}'
